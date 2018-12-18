82a3ac4ea9c93330cdfe1da25e109ff81c80009f
Peter Paravinja <paravinja92@gmail.com>
2018-12-18 13:41:22 +0100

    adding IsSolved boolian logic and state checker to see what the state was before and after running the strategy

---
+++ SudokuSolver/Workers/SudokuBoardStateManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver.Workers
+{
+    class SudokuBoardStateManager
+    {
+        public string GenerateState(int[,] sudokuBoard)
+        {
+            StringBuilder key = new StringBuilder();
+
+            //getlength v tem primeru izbere multidimentional array katerega želimo (prvi element ali drugi)
+            for (int row = 0; row < sudokuBoard.GetLength(0); row++)
+            {             
+                //getlength v tem primeru izbere multidimentional array katerega želimo (prvi element ali drugi) 
+                for (int col = 0; col < sudokuBoard.GetLength(1); col++)
+                {
+                    key.Append(sudokuBoard[row, col]);
+                }             
+            }
+
+            return key.ToString();
+        }
+
+        public bool IsSolver(int[,] sudokuBoard)
+        {
+            //getlength v tem primeru izbere multidimentional array katerega želimo (prvi element ali drugi)
+            for (int row = 0; row < sudokuBoard.GetLength(0); row++)
+            {
+                //getlength v tem primeru izbere multidimentional array katerega želimo (prvi element ali drugi) 
+                for (int col = 0; col < sudokuBoard.GetLength(1); col++)
+                {
+                    // pregled če je slučajno vrednost znotraj enaka 0 ali pa, če je dolžina števila večja kot 1 potem nekaj ni prav
+                    // sudoku ima lahko v enem elementu samo števila od 1-9 --- nesme biti npr 0 ali 1031 ali 96522542
+                    if(sudokuBoard[row, col] == 0 || sudokuBoard[row, col].ToString().Length > 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
