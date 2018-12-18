116514f9d45ea15f525396f6fee291b87f33bb1c
Peter Paravinja <paravinja92@gmail.com>
2018-12-18 13:29:17 +0100

    adding displayer

---
+++ SudokuSolver/Workers/SudokuBoardDisplayer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver.Workers
+{
+    class SudokuBoardDisplayer
+    {
+        public void Display(string title, int[,] sudokuBoard)
+        {
+            if (!title.Equals(string.Empty)) Console.WriteLine("{0} {1}", title, Environment.NewLine);
+
+            //getlength v tem primeru izbere multidimentional array katerega želimo (prvi element ali drugi)
+            for (int row = 0; row < sudokuBoard.GetLength(0); row++)
+            {
+                Console.Write("|");
+                //getlength v tem primeru izbere multidimentional array katerega želimo (prvi element ali drugi) 
+                for (int col = 0; col < sudokuBoard.GetLength(1); col++)
+                {
+                    Console.WriteLine("{0}{1}",sudokuBoard[row,col], "|");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
